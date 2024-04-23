73e58a4a00a04391f1696ca05bc4712ab1642a8e
fbweuibf <156813986+fbweuibf@users.noreply.github.com>
2024-04-23 17:50:09 -0400

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {
+xwindow.onscroll = function() {
 	make_sticky()
   };
   
@@ -13,3 +13,36 @@ window.onscroll = function() {
 	  navbar.classList.remove("sticky");
 	}
   }
+
+
+<script>
+
+var slideIndices = [1, 1, 1]; 
+var carouselClass = "carousel"; 
+
+function setupCarousels() {
+    // Initialize all carousels
+    var carousels = document.getElementsByClassName(carouselClass);
+    for (var i = 0; i < carousels.length; i++) {
+        showSlides(1, i); // Set the first slide to display for each carousel
+    }
+}
+
+function plusSlides(n, carouselNumber) {
+    // Change slide by n slides forward/backward in carouselNumber
+    showSlides(slideIndices[carouselNumber] += n, carouselNumber);
+}
+
+function showSlides(n, carouselNumber) {
+    var i;
+    var slides = document.querySelectorAll("." + carouselClass + "[data-carousel='" + carouselNumber + "'] .slide");
+    if (n > slides.length) { slideIndices[carouselNumber] = 1 }
+    if (n < 1) { slideIndices[carouselNumber] = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndices[carouselNumber] - 1].style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", setupCarousels);
+</script>
